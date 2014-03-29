c2a8f2a41f67daaa89152a196e57815661ee3773
jefferygleason <jeff.a.gleason@gmail.com>
2014-03-29 09:23:11 -0500

    Removed uname/pwd for mysql

--- config-default.php
+++ config-default.php
@@ -1,7 +1,7 @@
 <?php
 
 $config['atk']['base_path']='./atk4/';
-$config['dsn']='mysql://root:inc0that@localhost/lasthope';
+$config['dsn']='mysql://root:root@localhost/examples';
 
 $config['url_postfix']='';
 $config['url_prefix']='?page=';
